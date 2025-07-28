f81c24653a50943f099d27b53e7522d3b7e6c7dc
bonun00 <lifesciences00@naver.com>
2025-07-28 22:46:21 +0900

    feat(admin) service/AdminUserService 유저 정보 로직
    userId를 바탕으로 정보를 조회

--- src/main/java/app/domain/admin/service/AdminUserService.java
+++ src/main/java/app/domain/admin/service/AdminUserService.java
@@ -2,11 +2,16 @@ package app.domain.admin.service;
 
 import org.springframework.data.domain.Pageable;
 
+import app.domain.admin.model.dto.response.GetUserDetailResponse;
 import app.domain.admin.model.dto.response.GetUserListResponse;
+import app.domain.order.model.dto.response.GetOrderListResponse;
 import app.global.apiPayload.PagedResponse;
 
 public interface AdminUserService {
 
 	PagedResponse<GetUserListResponse> getAllUsers(Pageable pageable);
 
+	GetUserDetailResponse getUserDetailById(Long id);
+
+	PagedResponse<GetOrderListResponse> getUserOrderListById(Long userId, Pageable pageable);
 }
--- src/main/java/app/domain/admin/service/impl/AdminUserServiceImpl.java
+++ src/main/java/app/domain/admin/service/impl/AdminUserServiceImpl.java
@@ -1,14 +1,25 @@
 package app.domain.admin.service.impl;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import app.domain.admin.model.dto.response.GetUserDetailResponse;
 import app.domain.admin.model.dto.response.GetUserListResponse;
 import app.domain.admin.service.AdminUserService;
+import app.domain.order.model.dto.response.GetOrderListResponse;
+import app.domain.order.model.entity.Orders;
+import app.domain.order.model.mapper.OrdersRepository;
+import app.domain.user.model.UserAddressRepository;
 import app.domain.user.model.UserRepository;
+import app.domain.user.model.dto.response.GetUserAddressListResponse;
+import app.domain.user.model.entity.User;
 import app.domain.user.model.entity.enums.UserRole;
 import app.global.apiPayload.PagedResponse;
+import app.global.apiPayload.code.status.ErrorStatus;
+import app.global.apiPayload.exception.GeneralException;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -16,6 +27,8 @@ import lombok.RequiredArgsConstructor;
 public class AdminUserServiceImpl implements AdminUserService {
 
 	private final UserRepository userRepository;
+	private final UserAddressRepository userAddressRepository;
+	private final OrdersRepository ordersRepository;
 
 	@Override
 	public PagedResponse<GetUserListResponse> getAllUsers(Pageable pageable) {
@@ -24,4 +37,26 @@ public class AdminUserServiceImpl implements AdminUserService {
 
 		return PagedResponse.from(page);
 	}
+
+	@Override
+	public GetUserDetailResponse getUserDetailById(Long userId) {
+		User user = userRepository.findById(userId)
+			.orElseThrow(() -> new GeneralException(ErrorStatus.USER_NOT_FOUND));
+		List<GetUserAddressListResponse> addressList = userAddressRepository.findAllByUserUserId(userId)
+			.stream().map(GetUserAddressListResponse::from).toList();
+
+		return GetUserDetailResponse.from(user, addressList);
+	}
+
+	@Override
+	public PagedResponse<GetOrderListResponse> getUserOrderListById(Long userId, Pageable pageable) {
+		User user = userRepository.findById(userId)
+			.orElseThrow(() -> new GeneralException(ErrorStatus.USER_NOT_FOUND));
+
+		Page<Orders> ordersPage = ordersRepository.findAllByUserAndDeliveryAddressIsNotNull(user, pageable);
+		Page<GetOrderListResponse> mapped = ordersPage.map(GetOrderListResponse::from);
+
+		return PagedResponse.from(mapped);
+	}
+
 }
