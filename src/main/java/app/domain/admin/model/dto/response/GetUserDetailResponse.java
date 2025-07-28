be4adb82de057eafc6e7c62b0ba26729041739ae
bonun00 <lifesciences00@naver.com>
2025-07-28 22:54:27 +0900

    feat(Admin) model/dto/GetUserDetailResponse 관리자 유저 정보 조회
    유저의 자세한 정보 및 주소 목록을 담은 dto

---
+++ src/main/java/app/domain/admin/model/dto/response/GetUserDetailResponse.java
@@ -0,0 +1,34 @@
+package app.domain.admin.model.dto.response;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import app.domain.user.model.dto.response.GetUserAddressListResponse;
+import app.domain.user.model.entity.User;
+
+public record GetUserDetailResponse(
+	Long userId,
+	String email,
+	String name,
+	String nickName,
+	String phoneNumber,
+	LocalDateTime createdAt,
+	LocalDateTime updateAt,
+	List<GetUserAddressListResponse> address
+) {
+	public static GetUserDetailResponse from(
+		User user,
+		List<GetUserAddressListResponse> addressList
+	) {
+		return new GetUserDetailResponse(
+			user.getUserId(),
+			user.getEmail(),
+			user.getUsername(),
+			user.getNickname(),
+			user.getPhoneNumber(),
+			user.getCreatedAt(),
+			user.getUpdatedAt(),
+			addressList
+		);
+	}
+}
