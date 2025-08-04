b641a4cee4da1edf6e675bdce290b3b010325c80
Seunghyuk Choi <117180508+ColdTbrew@users.noreply.github.com>
2025-08-04 18:01:20 +0900

    chore(customer) : 안쓰는 principal 삭제

--- src/main/java/app/domain/customer/CustomerOrderController.java
+++ src/main/java/app/domain/customer/CustomerOrderController.java
@@ -2,8 +2,6 @@ package app.domain.customer;
 
 import java.util.List;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +24,6 @@ public class CustomerOrderController {
 	@Operation(summary = "고객 주문 내역 조회 API", description = "자신의 모든 주문 내역을 조회합니다.")
 	@GetMapping
 	public ApiResponse<List<CustomerOrderResponse>> getCustomerOrders(
-		@AuthenticationPrincipal UserDetails principal
 	) {
 		return ApiResponse.onSuccess(CustomerSuccessStatus.CUSTOMER_OK, customerOrderService.getCustomerOrders());
 	}
